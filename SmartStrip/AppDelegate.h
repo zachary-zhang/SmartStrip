439c009677d087d16a5edc2e5a29b2ee43ec9e04
john <john@zachanry.local>
2016-01-05 14:21:37 +0800

    Initial Commit

---
+++ SmartStrip/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SmartStrip
+//
+//  Created by john on 16/1/5.
+//  Copyright © 2016年 张摇奖. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SmartStrip/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmartStrip
+//
+//  Created by john on 16/1/5.
+//  Copyright © 2016年 张摇奖. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
